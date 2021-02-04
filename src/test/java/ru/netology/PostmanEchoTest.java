7d9b6367b7cf880acf02f248e72308b6e57c90e2
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2021-02-05 01:39:59 +0300

    Broken test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -1,25 +1,37 @@
 package ru.netology;
 
+import io.restassured.http.ContentType;
+import io.restassured.mapper.ObjectMapperType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
 
     @Test
     void PostmanEchoTest() {
+        JSONObject jsonObj = new JSONObject().put("Date", "05.02.2021");
+
         // Given - When - Then
 // Предусловия
+
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("application/json")
+                .body(jsonObj.toString()) // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-                .when()
+        .when()
                 .post("/post")
 // Проверки
-                .then()
+        .then()
+       //         .assertThat(from()
                 .statusCode(200)
-//                .body()
+                .body("data", equalTo("{Date=05.02.2021}"))
         ;
     }
 }
